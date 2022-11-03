c2a2fcd669a7852a845a3e7a85e59ca46cd15a3a
Juno Lee <0312juno@gmail.com>
2022-11-02 19:06:58 -0700

    "Add Contact Page"

---
+++ build-GroupProject-Desktop_Qt_5_12_12_clang_64bit-Debug/ui_contactus.h
@@ -0,0 +1,59 @@
+/********************************************************************************
+** Form generated from reading UI file 'contactus.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.12
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_CONTACTUS_H
+#define UI_CONTACTUS_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QTextBrowser>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_ContactUs
+{
+public:
+    QTextBrowser *textBrowser;
+
+    void setupUi(QDialog *ContactUs)
+    {
+        if (ContactUs->objectName().isEmpty())
+            ContactUs->setObjectName(QString::fromUtf8("ContactUs"));
+        ContactUs->resize(400, 300);
+        textBrowser = new QTextBrowser(ContactUs);
+        textBrowser->setObjectName(QString::fromUtf8("textBrowser"));
+        textBrowser->setGeometry(QRect(0, 0, 401, 301));
+
+        retranslateUi(ContactUs);
+
+        QMetaObject::connectSlotsByName(ContactUs);
+    } // setupUi
+
+    void retranslateUi(QDialog *ContactUs)
+    {
+        ContactUs->setWindowTitle(QApplication::translate("ContactUs", "Dialog", nullptr));
+        textBrowser->setHtml(QApplication::translate("ContactUs", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'.AppleSystemUIFont'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Contact Us:</p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">dingleberry119@Gmail.com</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">911 9111 9111</p></body></html>", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class ContactUs: public Ui_ContactUs {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_CONTACTUS_H
