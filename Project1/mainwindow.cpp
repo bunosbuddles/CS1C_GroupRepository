a763dc3b1a413b9bbeaaded455387c464ffbed1b
zekekim <ezekieljkim@gmail.com>
2022-11-07 12:29:20 -0800

    Add Parse and Read Functionality

--- Project1/mainwindow.cpp
+++ Project1/mainwindow.cpp
@@ -3,23 +3,29 @@
 #include "help.h"
 #include "contactus.h"
 #include "maintenance.h"
+#include "list.h"
+#include "filehandler.h"
+#include <QApplication>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    List *listPage = new List(parent);
+    ui->stackedWidget->insertWidget(0,listPage);
 
     Maintenance *maintenancePage = new Maintenance(parent);
-    ui->stackedWidget->insertWidget(0, maintenancePage);
+    ui->stackedWidget->insertWidget(1, maintenancePage);
 
     Help *helpPage = new Help(parent);
-    ui->stackedWidget->insertWidget(1, helpPage);
+    ui->stackedWidget->insertWidget(2, helpPage);
 
     ContactUs *contactUsPage = new ContactUs(parent);
-    ui->stackedWidget->insertWidget(2, contactUsPage);
+    ui->stackedWidget->insertWidget(3, contactUsPage);
 
-    ui->stackedWidget->setCurrentIndex(1);
+    ui->stackedWidget->setCurrentIndex(0);
+
 }
 
 MainWindow::~MainWindow()
@@ -30,18 +36,24 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_maintenanceBtn_clicked()
 {
-    ui->stackedWidget->setCurrentIndex(0);
+    ui->stackedWidget->setCurrentIndex(1);
 }
 
 
 void MainWindow::on_helpBtn_clicked()
 {
-    ui->stackedWidget->setCurrentIndex(1);
+    ui->stackedWidget->setCurrentIndex(2);
 }
 
 
 void MainWindow::on_contactUsBtn_clicked()
 {
-    ui->stackedWidget->setCurrentIndex(2);
+    ui->stackedWidget->setCurrentIndex(3);
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
 }
 
